7f51fbe4e0fab0b609d8d155c422fed43fc5da85
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-09-09 00:35:44 -0500

    Update StateProvider.js

--- src/datalayer/StateProvider.js
+++ src/datalayer/StateProvider.js
@@ -3,11 +3,13 @@ import React, { createContext, useContext, useReducer } from "react";
 // Preparing the data layer
 export const StateContext = createContext();
 
+//initial state is what data layer looks like when the app is loaded
+//reducer listens to changes in data layer
 export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
+    {children} {/*children is the app*/}
   </StateContext.Provider>
 );
 
-// Hook which allows us to pull information from the daya layer
+// Hook which allows you to pull information from the daya layer
 export const useStateProviderValue = () => useContext(StateContext);
